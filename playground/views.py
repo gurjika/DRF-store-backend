9587055265f293bf3f9ecdc47fced16dcd21c8bb
Luka <lgurjidze@gmail.com>
2024-02-15 18:19:27 +0400

    DRF - 2

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,12 @@ from django.core.mail import mail_admins, send_mail, BadHeaderError, EmailMessag
 from templated_mail.mail import BaseEmailMessage
 from .tasks import notify_customers
 import requests
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+from rest_framework.views import APIView
 
+@cache_page(timeout=5 * 60)
 def say_hello(request):
     # try:
     #     message = EmailMessage('subject', 'message', 'lgurjidze@gmail.com', ['luka.gurjidze04@gmail.com'])
@@ -20,6 +25,25 @@ def say_hello(request):
     # except BadHeaderError:
     #     pass
 
-    requests.get('https://httpbin.org/delay/2')
-    
-    return render(request, 'hello.html', {'name': 'Mosh'})
+
+
+    # key = 'httpbin_result'
+    # if cache.get(key) is None:
+    #     response = requests.get('https://httpbin.org/delay/2')
+    #     data = response.json()
+    #     cache.set(key, data, timeout=10 * 60)
+
+    response = requests.get('https://httpbin.org/delay/2')
+    data = response.json()
+
+    return render(request, 'hello.html', {'name': data})
+
+
+
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+
+        return render(request, 'hello.html', {'name': data})
