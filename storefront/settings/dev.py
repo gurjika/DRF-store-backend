928443297a52735d09bad75848c8d9a6f567ba76
Luka <lgurjidze@gmail.com>
2024-07-08 20:17:51 +0400

    configure environ variables

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -1,25 +1,31 @@
 from .common import *
+from dotenv import load_dotenv
 
+load_dotenv()
 
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
 DEBUG = True
 
+
+HOST = os.environ.get('HOST', '127.0.0.1')
+PASSWORD = os.environ.get('PASSWORD')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront3',
-        'HOST': 'localhost',
+        'NAME': 'storefront',
+        'HOST': HOST,
         'USER': 'root',
-        'PASSWORD': 'Lukikosi7680BGH',
+        'PASSWORD': PASSWORD,
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
@@ -28,7 +34,7 @@ CACHES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
